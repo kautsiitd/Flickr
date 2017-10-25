3e32705ac61c3e3de8e3041e60c5147fda0c4888
Kautsya Kanu <kautsiitd@gmail.com>
2017-10-26 00:19:45 +0530

    Removed unnecessary method

--- Flickr/Extensions/GradientView.swift
+++ Flickr/Extensions/GradientView.swift
@@ -8,18 +8,6 @@
 
 import UIKit
 
-extension UIView {
-	func applyGradient(colours: [UIColor], startPoint: CGPoint = CGPoint(x: 0, y: 0.5), endPoint: CGPoint = CGPoint(x: 1, y: 0.5), dynamicMaxWidth: CGFloat? = nil, dynamicMaxHeight: CGFloat? = nil) {
-		let gradient: CAGradientLayer = CAGradientLayer()
-		let frame = self.bounds
-		gradient.frame = CGRect(x: frame.minX, y: frame.minY, width: dynamicMaxWidth ?? frame.width, height: dynamicMaxHeight ?? frame.height)
-		gradient.colors = colours.map { $0.cgColor }
-		gradient.startPoint = startPoint
-		gradient.endPoint = endPoint
-		self.layer.insertSublayer(gradient, at: 0)
-	}
-}
-
 @IBDesignable
 class GradientView: UIView {
 	@IBInspectable var startColor: UIColor = UIColor.clear
